7415e0aaab493b5416284c985bffae187f255e6f
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2023-01-15 00:03:44 +0800

    Modify line 49

--- binary-tree.py
+++ binary-tree.py
@@ -46,6 +46,8 @@ def build_tree(elements):
     return root 
 
 if __name__ == '__main__':
-    numbers = [17, 4, 1, 20, 9 , 23, 18,34]
+    numbers = [17, 4, 1, 20, 9 , 23, 18, 34, 18, 4]
     numbers_tree = build_tree(numbers)
-    print(numbers_tree.in_order_traversal())        
+    print(numbers_tree.in_order_traversal())     
+    
+       
