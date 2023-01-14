b9bb323076023b885f27e51485efcef83d76efac
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2023-01-14 23:56:58 +0800

    Coded from line 24 to 38

--- binary-tree.py
+++ binary-tree.py
@@ -20,3 +20,20 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else: 
                 self.right = BinarySearchTreeNode(data)
+    
+    def in_order_traversal(self):
+        elements = []
+
+        # Visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # Visit base node
+        elements.append(self.data)
+
+        # Visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+        
+        return elements
+        
