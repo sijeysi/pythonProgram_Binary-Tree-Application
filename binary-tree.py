3bd9f375c5e41c21d8962f5f30b4687582d514b9
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2023-01-14 23:52:08 +0800

    Coded from line 7 to 22

--- binary-tree.py
+++ binary-tree.py
@@ -4,4 +4,19 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-    
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # Add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else: 
+                self.left = BinarySearchTreeNode(data)
+        else: 
+            # Add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else: 
+                self.right = BinarySearchTreeNode(data)
