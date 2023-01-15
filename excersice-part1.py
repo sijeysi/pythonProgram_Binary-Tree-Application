5dee0dbc5908c32be2d13bc7dcf7807187f7b1dd
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2023-01-15 08:02:59 +0800

    Mofidy the codes from binary-tree-part1.py

--- excersice-part1.py
+++ excersice-part1.py
@@ -1,2 +1,42 @@
 # Binary Tree Part 1 Exercise
 
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # Add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else: 
+                self.left = BinarySearchTreeNode(data)
+        else: 
+            # Add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else: 
+                self.right = BinarySearchTreeNode(data)
+    
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            # Val might be in left subtree
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data: 
+            # Val might be in right subtree
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
