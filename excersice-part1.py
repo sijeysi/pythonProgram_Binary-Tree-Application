8fc4272f709f6f6cc7a2c092bb091a973b5661dd
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2023-01-15 08:22:57 +0800

    Build Binary Tree

--- excersice-part1.py
+++ excersice-part1.py
@@ -70,3 +70,20 @@ class BinarySearchTreeNode:
             elements += self.right.post_order_traversal()
         elements.append(self.data)
         return elements
+
+# Build Binary Tree
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    return root 
+
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9 , 23, 18, 34, 18, 4]
+    numbers_tree = build_tree(numbers)
+
+    print("Input numbers:", numbers, "\n")
+    print("In order traversal:", numbers_tree.in_order_traversal())
+    print("Pre order traversal:", numbers_tree.pre_order_traversal())
+    print("Post order traversal:", numbers_tree.post_order_traversal())
