79bdba6364809914f67202ac99cfe8d0c1ac04f5
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2023-01-15 11:05:43 +0800

    Alter Filename

---
+++ BST-fullName-Application.py
@@ -0,0 +1,17 @@
+from binaryTree import BinarySearchTreeNode
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    return root 
+
+if __name__ == '__main__':
+    letters = ["C", "R", "Y", "S", "T", "A", "L", 
+                "J", "A", "N", "E",
+                "C", "A", "D", "I", "M", "A", "S"]
+    lettersName = build_tree(letters)
+
+    print("Full Name: CRYSTAL JANE CADIMAS")
+    
